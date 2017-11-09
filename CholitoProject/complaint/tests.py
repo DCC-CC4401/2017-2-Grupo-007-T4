710711c454a459ed7439f2eb9fe86a8e7b8e53c2
Gabriela <gabriela.mendoza@ug.uchile.cl>
2017-11-09 15:58:13 -0300

    not null test case

--- CholitoProject/complaint/tests.py
+++ CholitoProject/complaint/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from complaint.models import Complaint
 
-# Create your tests here.
+class ComplaintTestCase(TestCase):
+    def setUp(self):
+        Complaint.objects.create(descripction="Perro herido en la carretera", case ='1', lat=-33, lng= -70, directions= "Plaza de armas SN  Casilla 52D")
+
+    def test_not_null(self):
+        """Animals that can speak are correctly identified"""
+        perrito = Complaint.objects.get(name="Santiago")
+        self.assertTrue(perrito!=None)
