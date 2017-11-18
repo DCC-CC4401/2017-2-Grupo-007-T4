d0fe20c39af400621bd4c49d28fd4450e099d1ea
Gabriela <gabriela.mendoza@ug.uchile.cl>
2017-11-18 12:52:29 -0300

    avances en tests

--- CholitoProject/complaint/tests.py
+++ CholitoProject/complaint/tests.py
@@ -1,11 +1,16 @@
 from django.test import TestCase
-from complaint.models import Complaint
+from complaint.models import Complaint,AnimalType
+from municipality.models import Municipality, MunicipalityUser
 
 class ComplaintTestCase(TestCase):
     def setUp(self):
-        Complaint.objects.create(descripction="Perro herido en la carretera", case ='1', lat=-33, lng= -70, directions= "Plaza de armas SN  Casilla 52D")
+        muni= Municipality.objects.create(name="Santiago", lat=-33, lng= -70, directions= "Plaza de armas SN  Casilla 52D")
+        perro=AnimalType.objects.create(name="perro")
+        Complaint.objects.create(description="Perro herido en la carretera", case ="1", lat=-33, lng= -70,
+                                 directions= "Plaza de armas SN  Casilla 52D",status=1,animal_type=perro,
+                                 gender="1", wounded="1", color="2", municipality =muni)
 
     def test_not_null(self):
         """Animals that can speak are correctly identified"""
-        perrito = Complaint.objects.get(name="Santiago")
+        perrito = Complaint.objects.get(description="Perro herido en la carretera" )
         self.assertTrue(perrito!=None)
